6c4d741b3c8e72c1322f9e50108dfdd15e17fe1d
Tayyab Aslam <tayyab.aslam@sixlogics.local>
2022-10-11 17:39:24 +0500

    all code base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  
+  const name = 'user :)';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Todo Tracker !</h1>
+      <Header myCity = "Lahore" />
+      <h3>Welcome {name}</h3>  
     </div>
   );
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>
+//           Let's start React
+//         </h1>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+function Header(props) {
+    return (
+      <header>
+        <h1>___ TASK TRACKER ___</h1>
+        <p>I am living in  <b>{props.myCity}</b></p>
+      </header>
+    );
+}
+  
+
+export default Header;  
